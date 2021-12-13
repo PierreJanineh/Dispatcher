f35ef89b092f7b168721a59cee5b457f9304e9e2
Pierre Janineh <janinehpierre@gmail.com>
2021-12-13 18:27:34 +0200

    Splash screen Activity implementation

---
+++ app/src/main/java/com/example/dispatcher/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dispatcher
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewTreeObserver
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}

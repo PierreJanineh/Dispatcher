cfc8e6452ef267a560211adca07775321cc23050
Pierre Janineh <janinehpierre@gmail.com>
2021-12-20 15:42:39 +0200

    Renamed Application() and removed empty interceptor

--- app/src/main/java/com/example/dispatcher/model/DispatcherApplication.kt
+++ app/src/main/java/com/example/dispatcher/model/DispatcherApplication.kt
@@ -7,9 +7,8 @@ import com.example.dispatcher.model.network.ApiConstants.Companion.API_BASE_URL
 import com.example.dispatcher.model.network.ApiConstants.Companion.API_VERSION
 import com.example.dispatcher.model.network.ApiConfig
 import com.example.dispatcher.model.network.NetworkManager
-import okhttp3.Interceptor.Chain
 
-class Application: Application() {
+class DispatcherApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -19,9 +18,6 @@ class Application: Application() {
                 .Builder(API_BASE_URL)
                 .setAuthHeader(API_AUTH_KEY, API_AUTH_VALUE)
                 .setVersion(API_VERSION)
-                .addInterceptor { chain: Chain ->
-                    chain.proceed(chain.request())
-                }
                 .build()
         )
     }

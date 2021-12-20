ed2bbed6c5fc7d76643189a452afcfd03251e10b
Pierre Janineh <janinehpierre@gmail.com>
2021-12-20 15:50:55 +0200

    Replaced " " with "." for GET call

--- app/src/main/java/com/example/dispatcher/model/network/services/ArticleService.kt
+++ app/src/main/java/com/example/dispatcher/model/network/services/ArticleService.kt
@@ -7,10 +7,6 @@ import retrofit2.http.QueryMap
 
 
 interface ArticlesService {
-    /*
-    Space-only string in [@GET] for avoiding [java.lang.IllegalArgumentException]
-                   [Missing either @GET URL or @Url parameter.]
-     */
-    @GET(" ")
+    @GET(".")
     fun getArticles(@QueryMap params: Map<String, String>): Call<ArticlesResponse>
 }

6967853b0bba287444725785f569b273e52d4c3e
Pierre Janineh <janinehpierre@gmail.com>
2021-12-21 10:32:47 +0200

    Article class completion

--- app/src/main/java/com/example/dispatcher/model/Article.kt
+++ app/src/main/java/com/example/dispatcher/model/Article.kt
@@ -2,9 +2,17 @@ package com.example.dispatcher.model
 
 data class Article(
     val title: String?,
-    val imageUrl: String?,
+    val description: String?,
+    val url: String?,
+    val urlToImage: String?,
     val author: String?,
-    val body: String?
+    val body: String?,
+    val publishedAt: String?,
+    val content: String?,
 )
 
-data class ArticlesResponse(val status: String, val totalResults: Int, val articles: ArrayList<Article>)
+data class ArticlesResponse(
+    val status: String,
+    val totalResults: Int,
+    val articles: ArrayList<Article>
+)

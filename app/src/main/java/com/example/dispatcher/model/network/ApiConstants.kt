1c5f9607f7e9968e095f207f993e9fc66c1748e1
Pierre Janineh <janinehpierre@gmail.com>
2021-12-21 10:44:59 +0200

    Movement of BaseUrl and Version constants to DispatcherApplication.kt

--- app/src/main/java/com/example/dispatcher/model/network/ApiConstants.kt
+++ app/src/main/java/com/example/dispatcher/model/network/ApiConstants.kt
@@ -2,8 +2,6 @@ package com.example.dispatcher.model.network
 
 class ApiConstants {
     companion object {
-        const val API_BASE_URL = "https://newsapi.org/"
-        const val API_VERSION = "v2/"
         const val API_AUTH_KEY = "x-api-key"
         const val API_AUTH_VALUE = "cfc3dff320ca42e9bb30221934a89c5c"
     }

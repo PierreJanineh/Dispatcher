f563c4796dbcbde1d22e8ffb1bb071ceb2fd7437
Pierre Janineh <janinehpierre@gmail.com>
2021-12-21 12:48:03 +0200

    Moved Api constants to build variants

--- app/src/main/java/com/example/dispatcher/model/DispatcherApplication.kt
+++ app/src/main/java/com/example/dispatcher/model/DispatcherApplication.kt
@@ -1,13 +1,9 @@
 package com.example.dispatcher.model
 
 import android.app.Application
-import com.example.dispatcher.model.network.ApiConstants.Companion.API_AUTH_KEY
-import com.example.dispatcher.model.network.ApiConstants.Companion.API_AUTH_VALUE
+import com.example.dispatcher.BuildConfig.*
 import com.example.dispatcher.model.network.ApiConfig
 import com.example.dispatcher.model.network.NetworkManager
-
-const val API_BASE_URL = "https://newsapi.org/"
-const val API_VERSION = "v2/"
 
 class DispatcherApplication: Application() {
 
--- app/src/main/java/com/example/dispatcher/model/network/ApiConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dispatcher.model.network
-
-class ApiConstants {
-    companion object {
-        const val API_AUTH_KEY = "x-api-key"
-        const val API_AUTH_VALUE = "cfc3dff320ca42e9bb30221934a89c5c"
-    }
-}

a11a94254c49a46d880be6f0904d88fec8df8a1e
Pierre Janineh <janinehpierre@gmail.com>
2021-12-27 11:57:25 +0200

    Created main Navigation

--- app/src/main/java/com/example/dispatcher/view/main/MainActivity.kt
+++ app/src/main/java/com/example/dispatcher/view/main/MainActivity.kt
@@ -4,10 +4,11 @@ import android.view.LayoutInflater
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.ActionBar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.dispatcher.R
 import com.example.dispatcher.view.base.BaseActivity
 import com.example.dispatcher.databinding.ActivityMainBinding
-import com.example.dispatcher.view.fragments.SplashScreenFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun setup() {
@@ -34,22 +35,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         this.withBinding {
             this.root.setBackgroundResource(R.color.primary_color)
 
-            val trans = supportFragmentManager.beginTransaction()
-            trans.runOnCommit {
-                placeMainFragment()
-            }
-            trans.add(R.id.frame_layout, SplashScreenFragment()).commit()
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment)
+            val navController = (navHostFragment as NavHostFragment).navController
+
+            placeMainFragment(navController)
         }
     }
 
-    private fun placeMainFragment() {
-        val mainFragment = MainFragment()
-
+    private fun placeMainFragment(navController: NavController) {
         Handler(Looper.getMainLooper()).postDelayed({
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.frame_layout, mainFragment)
-                .commit()
+
+            navController.navigate(R.id.main_fragment)
             supportActionBar?.show()
 
             this.withBinding {
